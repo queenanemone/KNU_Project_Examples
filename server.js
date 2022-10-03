d0f532767599d228239bf31c0aea665c89e04fdd
queenanemone <shinhs9902@gmail.com>
2022-10-03 22:19:53 +0900

    주소에 값을 집어넣어서 출력할 수 있는 코드

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
-var https = require('http');
+var http = require('http');
+var url = require('url');
+var querystring = require('querystring');
 
-var server = https.createServer(function(request, response){
+var server = http.createServer(function(request, response){
+  console.log('--- log start ---');
+  var parsedUrl = url.parse(request.url);
+  console.log(parsedUrl);
+  var parsedQuery = querystring.parse(parsedUrl.query, '&', '=');
+  console.log('--- log end ---');
   response.writeHead(200, {'Content-Type':'text/html'});
   response.end('Hello node.js!!');
 });
