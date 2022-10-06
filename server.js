186af5bf0e2b14fbbcd6210511713ec889dac678
queenanemone <shinhs9902@gmail.com>
2022-10-06 15:18:04 +0900

    서버 GET, POST 형식으로 받아오는 코드. 자세한 내용은 index 참고

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
 var http = require('http');
-var url = require('url');
+var fs = require('fs');
 var querystring = require('querystring');
+const port = 3000;
 
 var server = http.createServer(function(request, response){
-  console.log('--- log start ---');
-  var parsedUrl = url.parse(request.url);
-  console.log(parsedUrl);
-  var parsedQuery = querystring.parse(parsedUrl.query, '&', '=');
-  console.log(parsedQuery);
-  console.log('--- log end ---');
-  response.writeHead(200, {'Content-Type':'text/html'});
-  response.end('Hello node.js!!');
-});
 
-server.listen(8080, function(){
+  if (request.method == 'GET'){
+    fs.readFile('./index.html', 'utf8', function(error, data){
+      //fs.readFile 함수를 이용, 현재 만들어진 html 파일인 index.html 파일을 localhost에 띄워준다
+      response.writeHead(200, {'Content-Type' : 'text/html'});
+      response.end(data);
+    });
+  }
+  else if(request.method == 'POST'){
+    //POST에서 들어온 아이디, 비밀번호를 chunk에 담고
+    request.on('data', function(chunk){
+      console.log(chunk.toString());
+      //chunk에 담은 데이터를 string, 문자열화 한 후
+      var data = querystring.parse(chunk.toString());
+      //querystring 안의 parse 함수를 이용하여 파싱한다. 
+      response.writeHead(200, {'Content-Type' : 'text/html'});
+      response.end('ID : ' + data.id + 'PW : ' + data.pw);
+    });
+  }
+}).listen(port, function(){
   console.log('Server is running...');
 });
